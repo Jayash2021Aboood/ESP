0442ac1fdb9a8a96f2aad46b20afefb91ca8136d
Aboood <AbdulmalekJayash@gmail.com>
2023-01-16 01:01:24 +0300

    Adding Footer file to master Page Template

---
+++ template/footer.php
@@ -0,0 +1,35 @@
+            <footer class="footer-admin mt-auto footer-light">
+                <div class="container-xl px-4">
+                    <div class="row">
+                        <div class="col-md-6 small">Copyright © Your Website 2021</div>
+                        <div class="col-md-6 text-md-end small">
+                            <a href="#!">Privacy Policy</a>
+                            ·
+                            <a href="#!">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!--  ======== End Content ============ -->
+            </div>
+            </div>
+            <script data-cfasync="false"
+                src="<?php echo $PATH_SERVER ?>/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js">
+            </script>
+            <script src="<?php echo $PATH_SERVER ?>js/bootstrap/bootstrap.bundle.min.js" crossorigin="anonymous">
+            </script>
+            <script src="<?php echo $PATH_SERVER ?>js/scripts.js"></script>
+            <script src="<?php echo $PATH_SERVER ?>js/simple-datatables/simple-datatables.js" crossorigin="anonymous">
+            </script>
+            <script src="<?php echo $PATH_SERVER ?>js/datatables/datatables-simple-demo.js"></script>
+            <script>
+$(document).ready(function() {
+    "use strict";
+    $('#successmessage').delay(3000).hide(1000);
+    $('#failmessage').delay(3000).hide(1000);
+
+});
+            </script>
+            </body>
+
+            </html>
