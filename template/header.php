a684d276e7aa137047b40837b1f1f9ec4ccc172e
Aboood <AbdulmalekJayash@gmail.com>
2023-01-16 00:58:22 +0300

    Cretae Template Folder to impelement Master Page Concept

---
+++ template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title><?php getTitle() ?></title>
+    <link href="<?php echo $PATH_SERVER ?>css/styles.css" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="<?php echo $PATH_SERVER ?>assets/img/favicon.png" />
+    <script data-search-pseudo-elements="" defer="" src="<?php echo $PATH_SERVER ?>js/font-awesome-6.1.1/all.min.js"
+        crossorigin="anonymous"></script>
+    <script src="<?php echo $PATH_SERVER ?>js/feather-icons/feather.min.js" crossorigin="anonymous"></script>
+    <script src="<?php echo $PATH_SERVER ?>js/jquery3.6.0.js"></script>
+</head>
+
+<body class="nav-fixed">
