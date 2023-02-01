0b13697934c6d317beb2d125b00fc117bf86ae33
Aboood <AbdulmalekJayash@gmail.com>
2023-02-01 07:03:43 +0300

    Update Customer Index Page

--- customer/index.php
+++ customer/index.php
@@ -1,18 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+  session_start();
+  include('../includes/lib.php');
+  $pageTitle = "Home";
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+  ?>
 
-<body>
-    <?php
-        phpinfo();
-    ?>
+<?php include('../template/header.php'); ?>
+<?php include('../template/startNavbar.php'); ?>
 
-</body>
+<main>
+    <header class="py-10  mb-4 bg-img-cover overlay overlay-60"
+        style="background-image: url('../assets/img/demo/demo-ocean-lg.jpg'); min-height: 500px; height: 500px;">
+        <div class="container-xl pt-10  px-4">
+            <div class="text-center  z-1">
+                <h1 class="text-white">Welcome to Engineer Services Provider</h1>
+                <p class="lead mb-0 text-white-50">A passion to deliver the best
+                </p>
+            </div>
+        </div>
+    </header>
+    <!-- Main page content-->
 
-</html>
+
+</main>
+
+
+
+<?php include('../template/footer.php') ?>
