f099ba11dd1fc4972cba549a9f3c52224e28a535
Aboood <AbdulmalekJayash@gmail.com>
2023-02-15 15:24:24 +0300

    adding Error report in page Header

--- engineerSignin.php
+++ engineerSignin.php
@@ -1,5 +1,7 @@
 <?php
   session_start();
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
   $pageTitle = "Signin as Engineer";
   include('includes/lib.php');
   include('includes/webuser.php');
