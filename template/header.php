d17b51d3674abfaa769f52857dc94ce997a44f38
Aboood <AbdulmalekJayash@gmail.com>
2023-02-18 06:40:53 +0300

    adding fontawesome in header file

--- template/header.php
+++ template/header.php
@@ -9,9 +9,21 @@
     <meta name="author" content="" />
     <title><?php getTitle() ?></title>
     <link href="<?php echo $PATH_SERVER ?>css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo $PATH_SERVER ?>assets/font-awesome/6.1.1/css/all.min.css" />
+    <!-- <link href="<?php //echo $PATH_SERVER ?>assets/fontawesome-6.3.0/css/fontawesome.min.css" rel="stylesheet" /> -->
+    <!-- <link href="<?php //echo $PATH_SERVER ?>assets/fontawesome-6.3.0/css/all.min.css" rel="stylesheet" /> -->
+
+
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
     <link href="<?php echo $PATH_SERVER ?>css/custom.css" rel="stylesheet" />
     <link rel="icon" type="image/x-icon" href="<?php echo $PATH_SERVER ?>assets/img/favicon.png" />
     <link href="https://cdn.jsdelivr.net/npm/litepicker/dist/css/litepicker.css" rel="stylesheet" />
+
+
+    <!-- <script data-search-pseudo-elements="" defer="" src="<?php //echo $PATH_SERVER ?>js/font-awesome-6.1.1/all.min.js" -->
+    <!-- crossorigin="anonymous"></script> -->
+
+    <!-- <script defer="" src="<?php //echo $PATH_SERVER; ?>js/fontawesome-6.3.0/all.min.js"></script> -->
     <script data-search-pseudo-elements="" defer="" src="<?php echo $PATH_SERVER ?>js/font-awesome-6.1.1/all.min.js"
         crossorigin="anonymous"></script>
     <script src="<?php echo $PATH_SERVER ?>js/feather-icons/feather.min.js" crossorigin="anonymous"></script>
