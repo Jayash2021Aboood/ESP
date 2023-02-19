ec47e2349839927cdef5deda19de59083fdf8f6c
Aboood <AbdulmalekJayash@gmail.com>
2023-02-20 02:55:00 +0300

    adding Attachment To  customer booking_details file

--- customer/booking_details.php
+++ customer/booking_details.php
@@ -325,6 +325,263 @@
 
         </div>
     </div>
+
+    <header class="pt-5 mt-4 mb-0 ">
+        <div class="container-xl  text-center">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-center ">
+                        <h1 class="text-primary">Booking Attachments</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <hr class="mb-1" />
+    <!-- Main page content-->
+    <div class="container-xl px-4">
+        <div class="row">
+            <div class="card border-start-lg border-start-primary card-header-actions card-scrollable">
+                <div class="card-header border-bottom ">
+                    <ul class="nav nav-tabs card-header-tabs" id="cardTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="overview-tab" href="#overview" data-bs-toggle="tab"
+                                role="tab" aria-controls="overview" aria-selected="true">Overview</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="attachments-tab" href="#attachments" data-bs-toggle="tab" role="tab"
+                                aria-controls="attachments" aria-selected="false">Attachments</a>
+                        </li>
+                    </ul>
+                    <div>
+                        <?php
+                            if($row['state'] == 'request')
+                                echo /*html*/'<span class="badge bg-gray-600">'.$row['state'].'</span>';
+                            else if($row['state'] == 'reject')
+                                echo /*html*/'<span class="badge bg-red ">'.$row['state'].'</span>';
+                            else if($row['state'] == 'working')
+                                echo /*html*/'<span class="badge bg-purple ">'.$row['state'].'</span>';
+                            else if($row['state'] == 'ready')
+                                echo /*html*/'<span class="badge bg-blue">'.$row['state'].'</span>';
+                            else if($row['state'] == 'done')
+                                echo /*html*/'<span class="badge bg-green">'.$row['state'].'</span>';
+                            else if($row['state'] == 'paid')
+                                echo /*html*/'<span class="badge bg-yellow-soft text-yellow">'.$row['state'].'</span>';
+                            else if($row['state'] == 'canceled')
+                                echo /*html*/'<span class="badge bg-red-soft text-red">'.$row['state'].'</span>';
+
+                            ?>
+                    </div>
+                </div>
+                <div class="card-body p-5">
+                    <div class="tab-content" id="cardTabContent">
+                        <div class="tab-pane fade show active" id="overview" role="tabpanel"
+                            aria-labelledby="overview-tab">
+                            <h5 class="card-title "> Booking NO: <?php echo $row['id']; ?></h5>
+                            <h5 class="card-title "> Booking Date: <?php echo $row['booking_date']; ?>
+                            </h5>
+                            <h5 class="card-title text-primary">Service Name : <?php echo $service['name']; ?></h5>
+                            <h5>Service Price : R.S <?php echo $row['service_price']; ?></h5>
+                            <h5>Paid Price : R.S <?php echo $row['paid_price'] ?? 0; ?></h5>
+                            <div class="text-s fw-bold d-inline-flex align-items-center">
+                                <?php echo $row['detail']; ?>
+                            </div>
+                            <p class="card-text"> Provided By:
+                                <span
+                                    class="text-success"><?php echo $engineer['first_name'] . ' ' . $engineer['last_name'];?></span>
+                            </p>
+                            <?php if( $row['state'] == 'ready' || $row['state'] == 'working'){  ?>
+                            <form class="d-inline-block mb-2" action="bookingManager.php" method="POST"
+                                enctype="multipart/form-data">
+                                <!-- Form Group (booking_id)-->
+                                <input type="hidden" name="booking_id" id="booking_id" value="<?php echo $row['id'];?>"
+                                    required>
+                                </input>
+                                <!-- Submit button-->
+                                <?php if( $row['state'] == 'ready'){  ?>
+                                <button name="customerAcceptBookingDone" class="btn btn-success" type="submit">
+                                    Service Completed</button>
+                                <button name="customerBackBookingToEnginner" class="btn btn-primary" type="submit">
+                                    Back To Engineer</button>
+                                <?php } ?>
+                                <button name="customerCanceledBooking" class="btn btn-danger" type="submit">
+                                    Cancel Service</button>
+                            </form>
+
+                            <!-- <form class="d-inline-block mb-2" action="bookingManager.php" method="POST"
+                                enctype="multipart/form-data"> -->
+                            <!-- Form Group (booking_id)-->
+                            <!-- <input type="hidden" name="booking_id" id="booking_id" value="<?php //echo $row['id'];?>"
+                                    required>
+                                </input> -->
+                            <!-- Submit button-->
+                            <!-- </form> -->
+                            <?php } ?>
+                        </div>
+                        <!--  Start Payment Tab  -->
+                        <div class="tab-pane fade" id="payment" role="tabpanel" aria-labelledby="payment-tab">
+                            <h5 class="card-title">Attachments</h5>
+                            <p class="card-text"></p>
+                            <form action="bookingManager.php" method="POST" enctype="multipart/form-data">
+                                <!-- Form Row-->
+                                <div class="row gx-3 mb-3">
+                                    <!-- Form Group (engineer_id)-->
+
+                                    <input type="hidden" name="id" id="id" value="<?php echo $row['id'];?>" required>
+                                    </input>
+
+                                    <input class="form-control" type="hidden" name="engineer_id" id="engineer_id"
+                                        value="<?php echo $engineer['id'];?>" required>
+                                    </input>
+                                    <!-- Form Group (service_id)-->
+                                    <input type="hidden" name="service_id" id="service_id"
+                                        value="<?php echo $row['service_id'];?>" required>
+                                    </input>
+
+
+                                    <!-- Form Group (card_number)-->
+                                    <div class="col-md-4 mb-3">
+                                        <label class="small mb-1" for="card_number">Card Number</label>
+                                        <input class="form-control" id="card_number" name="card_number" type="text"
+                                            placeholder="Card Number" value="<?php echo $row['card_number'];?>" />
+                                    </div>
+                                    <!-- Form Group (service_price)-->
+                                    <div class="col-md-4 mb-3">
+                                        <label class="small mb-1" for="service_price">Service Price</label>
+                                        <input class="form-control" id="service_price" name="service_price" type="text"
+                                            placeholder="Service Price" value="<?php echo $row['service_price'];?>"
+                                            readonly />
+                                    </div>
+                                    <!-- Form Group (paid_price)-->
+                                    <div class="col-md-4 mb-3">
+                                        <label class="small mb-1" for="paid_price">Paid Price</label>
+                                        <input class="form-control" id="paid_price" name="paid_price" type="text"
+                                            placeholder="Paid Price" value="<?php echo $row['paid_price'];?>" />
+                                    </div>
+                                    <!-- Form Group (detail)-->
+                                    <div class="col-md-4 mb-3">
+                                        <label class="small mb-1" for="detail">Detail</label>
+                                        <input class="form-control" id="detail" name="detail" type="text"
+                                            placeholder="Detail" value="<?php echo $row['detail'];?>" readonly />
+                                    </div>
+                                    <!-- Form Group (booking_date)-->
+                                    <div class="col-md-4 mb-3">
+                                        <label class="small mb-1" for="booking_date">Booking Date</label>
+                                        <input class="form-control" id="booking_date" name="booking_date" type="Date"
+                                            placeholder="Booking Date" value="<?php echo $row['booking_date'];?>"
+                                            readonly />
+                                    </div>
+                                </div>
+                                <!-- Submit button-->
+                                <button name="customerPaidBooking" class="btn btn-success"
+                                    type="submit">Payment</button>
+                            </form>
+                        </div>
+                        <!-- End Payment Tab -->
+
+
+                        <?php if( !($row['state'] == 'request' || $row['state'] == 'reject' ) ){  ?>
+                        <!--  Start Attachments Tab  -->
+                        <div class="tab-pane fade h-100" id="attachments" role="tabpanel"
+                            aria-labelledby="attachments-tab">
+
+                            <?php if($row['state'] == 'done' || $row['state'] == 'paid' ){  ?>
+                            <!-- Rating Form -->
+                            <form action="bookingManager.php" method="POST" enctype="multipart/form-data">
+                                <!-- Form Row-->
+                                <div class="row gx-3 mb-3">
+                                    <!-- Form Group (customer_id)-->
+
+                                    <input class="form-control" type="hidden" name="customer_id" id="customer_id"
+                                        value="<?php if(isCustomer()) echo $_SESSION['userID'];?>" required>
+                                    </input>
+                                    <!-- Form Group (booking_id)-->
+                                    <input type="hidden" name="booking_id" id="booking_id"
+                                        value="<?php echo $row['id'];?>" required>
+                                    </input>
+
+
+                                    <!-- Form Group (rate)-->
+                                    <div class="col-10 mb-3">
+                                        <!-- <label class="small mb-1" for="note">Note</label> -->
+                                        <input class="form-control" id="rate" name="rate" type="range" min="0" max="100"
+                                            value="20" required oninput="this.nextElementSibling.value = this.value" />
+                                        <output>20</output>
+                                    </div>
+                                    <div class="col-2">
+
+                                        <!-- Submit button-->
+                                        <button name="customerAddRating" class="btn btn-success"
+                                            type="submit">Rate</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <!-- End Rating Form -->
+                            <?php } ?>
+
+                            <?php if($row['state'] != 'canceled'){  ?>
+                            <form action="bookingManager.php" method="POST" enctype="multipart/form-data">
+                                <!-- Form Row-->
+                                <div class="row gx-3 mb-3">
+                                    <!-- Form Group (customer_id)-->
+
+                                    <input class="form-control" type="hidden" name="customer_id" id="customer_id"
+                                        value="<?php if(isCustomer()) echo $_SESSION['userID'];?>" required>
+                                    </input>
+                                    <!-- Form Group (booking_id)-->
+                                    <input type="hidden" name="booking_id" id="booking_id"
+                                        value="<?php echo $row['id'];?>" required>
+                                    </input>
+
+
+                                    <!-- Form Group (note)-->
+                                    <div class="col-10 mb-3">
+                                        <!-- <label class="small mb-1" for="note">Note</label> -->
+                                        <input class="form-control" id="note" name="note" type="text"
+                                            placeholder="Enter Your Note" value="" required />
+                                    </div>
+
+                                    <div class="col-2">
+
+                                        <!-- Submit button-->
+                                        <button name="customerAddBookingNote" class="btn btn-success"
+                                            type="submit">Send</button>
+
+                                    </div>
+                                </div>
+                            </form>
+                            <?php }  ?>
+
+
+
+                            <div class="col-12 noteMessage">
+                                <?php 
+                                    $allAttachments = getAllBookingNote($row['id']);
+                                    if(count($allAttachments ) > 0)
+                                    {
+                                        foreach($allAttachments as $note)
+                                        {                                    
+                                ?>
+                                <?php if(isCustomer() && $note['customer_id'] == $_SESSION['userID']){ ?>
+                                <div>
+                                    <p class="bg-blue-soft p-2 rounded-1 d-inline-block"><?php echo $note['note'];?></p>
+                                </div>
+                                <?php } else {?>
+                                <div style="text-align: right;">
+                                    <p class="bg-cyan-soft p-2 rounded-1 d-inline-block"><?php echo $note['note'];?></p>
+                                </div>
+                                <?php } ?>
+                                <?php } }?>
+                            </div>
+                        </div>
+                        <!-- End Attachments Tab -->
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </main>
 
 
